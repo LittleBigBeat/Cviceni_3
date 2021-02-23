f7e28ada6a3e01290cff8efee2655a427385864d
LittleBigBeat <78912172+LittleBigBeat@users.noreply.github.com>
2021-02-23 22:57:21 +0100

    Update main.cs

--- cviceni/cviceni/main.cs
+++ cviceni/cviceni/main.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace cviceni
 {
     public class Program
-    {
+    
     public static void Main(string[] args)
         {     
         double[,] matice1 = new double[,]
